ef98c028b763d8b6988a5a8fbcd46547867a00ff
Даниил Михайлович Володин <dvolodin@ufs.travel>
2023-04-15 14:06:24 +0300

    Добавьте файлы проекта.

---
+++ OtusHw13/EnumerableExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusHw13
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> source, int percentage)
+        {
+            if (percentage < 1 || percentage > 100)
+            {
+                throw new ArgumentException("Процент должен быть в диапазоне от 1 до 100.");
+            }
+
+            int count = source.Count();
+            int topCount = (int)Math.Ceiling(count * percentage / 100.0);
+            if (topCount == 0)
+            {
+                return Enumerable.Empty<T>();
+            }
+
+            return source.OrderByDescending(x => x).Take(topCount);
+        }
+
+        public static IEnumerable<T> Top<T, TKey>(this IEnumerable<T> source, int percentage, Func<T, TKey> keySelector)
+        {
+            if (percentage < 1 || percentage > 100)
+            {
+                throw new ArgumentException("Процент должен быть в диапазоне от 1 до 100.");
+            }
+
+            int count = source.Count();
+            int topCount = (int)Math.Ceiling(count * percentage / 100.0);
+            if (topCount == 0)
+            {
+                return Enumerable.Empty<T>();
+            }
+
+            return source.OrderByDescending(keySelector).Take(topCount);
+        }
+    }
+}
---
+++ OtusHw13/Program.cs
@@ -0,0 +1,24 @@
+﻿using OtusHw13;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var listPerson = new List<Person>()
+        {
+            new Person(){Name = "Alex", Age = 11},
+            new Person(){Name = "Felix", Age = 21},
+            new Person(){Name = "Ivan", Age = 31},
+            new Person(){Name = "Nicola", Age = 41},
+            new Person(){Name = "Julia", Age = 51},
+        };
+        list = list.Top(30).ToList();
+        list.ForEach(Console.Write);
+        Console.WriteLine();
+        listPerson = listPerson.Top(30, person => person.Age).ToList();
+        listPerson.ForEach(x => Console.WriteLine($"Name - {x.Name}. Age - {x.Age}."));
+        Console.ReadLine();
+    }
+}
